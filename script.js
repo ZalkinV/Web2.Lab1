5b13a1c38ec0138172f3d72ff40c126b65b8d481
Zalkin <Zalkin.viktor@mail.ru>
2019-09-15 12:39:21 +0300

    Add getting data from weather API for London

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 "use strict"
 
 
-var data = { title: "My New Post", body: "This is my first post!" };
-displayWeather(data);
+let request = new XMLHttpRequest();
+request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7825ce4ffa896c5019e53087c858568a")
+request.responseType = "json";
+request.onload = function()
+{
+    if (request.status == 200)
+        displayWeather(request.response)
+    else
+        document.body.innerHTML += request.statusText;
+}
+request.send();
+
 
 function displayWeather(weatherParams)
 {
-    var source = document.getElementById("entry-template").innerHTML;
+    var source = document.getElementById("weather-template").innerHTML;
     var template = Handlebars.compile(source);
     
     var html = template(weatherParams);

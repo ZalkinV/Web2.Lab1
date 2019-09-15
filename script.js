31e281c921de638872d1ff24feec9228def5ca4f
Zalkin <Zalkin.viktor@mail.ru>
2019-09-15 13:43:18 +0300

    Add button and input field

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 "use strict"
 
 
-let request = new XMLHttpRequest();
-request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7825ce4ffa896c5019e53087c858568a")
-request.responseType = "json";
-request.onload = function()
+function buttonClick()
 {
-    if (request.status == 200)
-        displayWeather(request.response)
-    else
-        document.body.innerHTML += request.statusText;
+    let cityName = document.getElementById("input").value;
+
+    let request = new XMLHttpRequest();
+    request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7825ce4ffa896c5019e53087c858568a");
+    request.responseType = "json";
+    request.onload = function()
+    {
+        if (request.status == 200)
+            displayWeather(request.response)
+        else
+            document.body.innerHTML += request.statusText;
+    }
+    request.send();
 }
-request.send();
 
 
 function displayWeather(weatherParams)

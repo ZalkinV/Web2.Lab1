07838b35795ac667fd7eeda29e80dc3b4072079a
Zalkin <Zalkin.viktor@mail.ru>
2019-11-08 01:57:03 +0300

    JS. Create display.js with displaying functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import getWeather from "./api";
-import weatherTemplate from "handlebars-loader!../templates/weather.hbs";
-import errorTemplate from "handlebars-loader!../templates/error.hbs";
-
-
-const WEATHER_DIV = document.getElementById("weather-container");
-const ERROR_DIV = document.getElementById("error-container");
+import { displayWeather, displayError, updateTab } from "./display";
 
 
 window.onload = () => {
@@ -35,25 +30,6 @@ function onSubmit(e) {
         error => displayError(error));
 }
 
-
-function displayWeather(weather) {
-    const weatherHtml = weatherTemplate(weather);
-
-    ERROR_DIV.innerHTML = "";
-    WEATHER_DIV.innerHTML = weatherHtml;
-}
-
-function displayError(message) {
-    const errorHtml = errorTemplate({ message });
-
-    ERROR_DIV.innerHTML = errorHtml;
-    WEATHER_DIV.innerHTML = "";
-}
-
-function updateTab(newTitle, newImage) {
-    document.title = newTitle;
-    document.getElementById("favicon").href = newImage;
-}
 
 function extractForecast(response) {
     const ICON_SIZE = 64;

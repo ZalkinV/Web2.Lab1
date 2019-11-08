b0d3cfd19adaf9f41369409b55d9bbe64e9135b4
Zalkin <Zalkin.viktor@mail.ru>
2019-11-08 03:03:42 +0300

    tests. Create display.tetst.js with simple test

---
+++ tests/display.test.js
@@ -0,0 +1,12 @@
+const assert = require("chai").assert;
+
+
+describe("Display", function() {
+    it("true", function() {
+        assert.isOk(true);
+    });
+
+    it("false", function() {
+        assert.isAbove(1, 0);
+    });
+});
